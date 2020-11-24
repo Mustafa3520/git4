45f3474bfaf442c8e4fb658a11f1387d3fa49cfb
Mustafa Aydin <mustafakilic3520@gmail.com>
2020-11-24 22:49:48 +0300

    ekledim cıkardım

--- src/git4.java
+++ src/git4.java
@@ -1,14 +1,16 @@
 public class git4 {
     public static void main(String[] args) {
         System.out.println("git4 ignoreler");
+        System.out.println("en son ekledim mi");
     }
 
     public static void  yenımetod(){
 
-        System.out.println(" yeni metod hazır mıııı????????????????");
+        System.out.println(" metod hzır mıııı???");
         System.out.println("sepete ekle");
     }
     public static void commitPushdegildir(){
         System.out.println(" tamam mı");
     }
+
 }
