0b36690ba11766b5cef90bcb06c38a2227a0d829
Mustafa Aydin <mustafakilic3520@gmail.com>
2020-11-24 22:33:43 +0300

    sepete ekleme kısmı yapıldı

--- src/git4.java
+++ src/git4.java
@@ -2,4 +2,10 @@ public class git4 {
     public static void main(String[] args) {
         System.out.println("git4 ignoreler");
     }
+
+    public static void  yenımetod(){
+
+        System.out.println(" yeni metod hazır");
+        System.out.println("sepete ekle");
+    }
 }
